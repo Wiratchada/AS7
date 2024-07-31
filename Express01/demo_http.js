e9327feba0bbb63b733cbd33e8286a080cb37dd6
Wiratchada <140487595+Wiratchada@users.noreply.github.com>
2024-07-31 15:45:13 +0700

    as7

---
+++ Express01/demo_http.js
@@ -0,0 +1,10 @@
+let http = require('http');
+let url = require('url');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+/*Use the url module to turn the querystring into an object:*/
+var q = url.parse(req.url, true).query;
+/*Return the year and month from the query object:*/
+var txt = q.year + " " + q.month;
+res.end(txt);
+}).listen(9999);
